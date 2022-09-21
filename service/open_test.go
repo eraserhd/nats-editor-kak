c96b9c954325687e2f900cc08fdd5bdb4aa3ac5f
Jason Felice <jason.m.felice@gmail.com>
2022-09-21 08:39:54 -0400

    Refactor test

--- service/open_test.go
+++ service/open_test.go
@@ -8,14 +8,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Open_uses_editor_session_when_sent(t *testing.T) {
+type openOption = func(msg *nats.Msg)
+
+func session(name string) openOption {
+	return func(msg *nats.Msg) {
+		msg.Header["Session"] = []string{name}
+	}
+}
+
+func open(t *testing.T, opts ...openOption) OpenCmd {
 	s, err := New()
 	require.NoError(t, err)
-	cmd := s.OpenCommand(&nats.Msg{
-        	Subject: "editor.open",
-        	Header: map[string][]string{
-                	"Session": {"foo"},
-        	},
-	})
-	assert.Equal(t, "foo", cmd.Session)
+	msg := &nats.Msg{
+		Subject: "editor.open",
+		Header: map[string][]string{
+			"Session": {"editorsession"},
+		},
+	}
+	for _, opt := range opts {
+		opt(msg)
+	}
+	return s.OpenCommand(msg)
+}
+
+func Test_Open_uses_editor_session_when_sent(t *testing.T) {
+	assert.Equal(t, "foo", open(t, session("foo")).Session)
 }
