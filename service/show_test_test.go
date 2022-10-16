b6d8bfaa53a854d155dc18e330e408256912d314
Jason Felice <jason.m.felice@gmail.com>
2022-10-16 12:45:41 -0400

    Default client to jumpclient

--- service/show_test_test.go
+++ service/show_test_test.go
@@ -6,7 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func (r runResult) showTextScript() *showText {
+	script, ok := r.kakouneCommand().Script.(*showText)
+	if !ok {
+		r.t.Fatalf("wanted script to be of type *showText, got %T", r.kakouneCommand().Script)
+	}
+	return script
+}
+
 func Test_sends_current_session_name(t *testing.T) {
 	sess := run(t, "cmd.show.data.text").kakouneCommand().Session
 	assert.Equal(t, "this_session", sess)
+}
+
+func Test_defaults_client_to_jumpclient(t *testing.T) {
+	client := run(t, "cmd.show.data.text").showTextScript().Client
+	assert.Equal(t, "%opt{jumpclient}", client)
 }
--- service/show_text.go
+++ service/show_text.go
@@ -4,9 +4,20 @@ import (
 	"github.com/plugbench/kakoune-pluggo/kakoune"
 )
 
+type showText struct {
+	Client string
+}
+
+func (s *showText) String() string {
+	return "" //FIXME:
+}
+
 func executeShowText(act *action) {
 	cmd := kakoune.Command{
 		Session: act.kakouneSession,
+		Script: &showText{
+			Client: "%opt{jumpclient}",
+		},
 	}
 	act.runKakouneScript(cmd)
 }
